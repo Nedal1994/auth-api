93e6e9ebd623d5f58a5fe58e826ce39016339d06
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-12-15 22:46:52 +0200

    ok

--- src/routes/v2.js
+++ src/routes/v2.js
@@ -1,21 +1,27 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const bearer = require('../middleware/bearer')
-const router = express.Router()
-const acl = require('../middleware/acl')
+const express = require("express");
+const bearer = require("../middleware/bearer");
+const router = express.Router();
+const acl = require("../middleware/acl");
 
-router.post('/:model',bearer,acl('create'),(req,res)=>{
-    res.status(200).send('img is created')
-  })
-  router.get('/:model',bearer,acl('read'),(req,res)=>{
-    res.status(200).send('img is read')
-  })
-  router.put('/:model',bearer,acl('update'),(req,res)=>{
-    res.status(200).send('img is updated')
-  })
-  router.delete('/:model',bearer,acl('delete'),(req,res)=>{
-    res.status(200).send('img is deleted')
-  })
+router.get("/:model", bearer, acl("read"), (req, res) => {
+  res.status(200).send("you can read the data");
+});
+router.post("/:model", bearer, acl("create"), (req, res) => {
+  res.status(200).send("you can create the data");
+});
 
-  module.exports=router
+router.put("/:model/:id", bearer, acl("update"), (req, res) => {
+  res.status(200).send("you can update the data");
+});
+
+router.delete("/:model/:id", bearer, acl("delete"), (req, res) => {
+  res.status(200).send("you can delete the data");
+});
+
+router.patch("/:model/:id", bearer, acl("update"), (req, res) => {
+  res.status(200).send("you can update the data");
+});
+
+module.exports=router
